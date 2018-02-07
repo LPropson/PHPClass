dd8742fcd9fe18d9656d34b502417ae3dc5800f2
LPropson <120088854@fvtc.edu>
2018-02-07 17:53:45 -0600

    Commit First time

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Class - Larry Propson</title>
+    <link rel="stylesheet" type="text/css" href="css/mystyles.css"/>
+</head>
+
+<body>
+<div class="page-wrap">
+    <header class="site-header">
+        <h1>PHP Class - Larry</h1>
+    </header>
+
+    <div class="flex-box">
+        <vav class="main-nav">
+            <ul>
+                <li class="menu-item"><a href="/">Home</a></li>
+                <li class="menu-item"><a href="#">Loops</a></li>
+                <li class="menu-item"><a href="#">Strings</a></li>
+            </ul>
+
+        </vav>
+
+        <section class="main-content">
+            <div class="img"><img src/></div>
+            <h2>Hello!</h2>
+            <div class="content">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                    the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                    of type and scrambled it to make a type specimen book.</p>
+
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                    the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                    of type and scrambled it to make a type specimen book.</p>
+            </div>
+        </section>
+
+        <aside class="right-sidebar">Sidebar</aside>
+    </div><!-- /Flex-box here -->
+    <footer>
+        <div>Learn PHP<sup>&copy;</sup> 2018</div>
+    </footer>
+
+</div><!-- /page-wrap -->
+
+
+</body>
+</html>
