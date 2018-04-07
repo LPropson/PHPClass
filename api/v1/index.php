e31700a370706df279d3d48e9b4e194323e2908d
LPropson <120088854@fvtc.edu>
2018-04-07 18:35:07 -0500

    Commit All this time

---
+++ api/v1/index.php
@@ -0,0 +1,20 @@
+<?php
+require 'Slim/Slim.php';
+\Slim\Slim:registerAutoloader();
+
+ /* Put = Update
+ * Post = Insert
+ * Get = Select
+ * Delete = Delete
+ */
+
+ $app = new \Slim\Slim();
+
+ $app->get('/getHello','getHello');
+ $app->run();
+
+ function getHello(){
+     echo "Hello World";
+ }
+
+?>
